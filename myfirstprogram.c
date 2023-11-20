e16e38bff8c614b79422a82a2999651ef9dae07d
danielecarpentiero <carpentierodaniele@gmail.com>
2023-11-21 00:16:51 +0100

    feat: my second commit in C

--- myfirstprogram.c
+++ myfirstprogram.c
@@ -1,8 +1,26 @@
-#include <stdio.h>
+/* #include <stdio.h>
 
 int main()
-
 {
-    printf("Hello World!\nI am learning C.\nAnd it is awesome!");
-    return 0;
+    int firstNumber = 17;
+    int secondNumber = 22;
+    int sum = firstNumber + secondNumber;
+    printf("the sum of %d and %d is %d", firstNumber, secondNumber, sum);
+} */
+
+#include <stdio.h>
+int main()
+{
+
+    /* student data */
+    int studentID = 25;
+    int studentAge = 23;
+    float studentFee = 75.25;
+    char studentGrade = 'B';
+
+    /* print variables */
+    printf("The student id is: %d\n", studentID);
+    printf("The student age is %d\n", studentAge);
+    printf("The student fee is %f\n", studentFee);
+    printf("The student grade is %c", studentGrade);
 }
